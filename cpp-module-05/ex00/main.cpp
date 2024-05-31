8e6d30ef7f5019f79ee88200c2afe545c46c3805
Loup Quehec <lquehec@bess-f3r4s3.clusters.42paris.fr>
2024-05-31 18:12:45 +0200

    init

---
+++ cpp-module-05/ex00/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lquehec <lquehec@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 16:30:48 by lquehec           #+#    #+#             */
+/*   Updated: 2024/05/31 18:10:34 by lquehec          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int	main(void)
+{
+	Bureaucrat *bureaucrat = new Bureaucrat("Bureaucrat", 150);
+	std::cout << *bureaucrat << std::endl;
+	// decremetGrade => exception "Grade is too low"
+	try {
+		bureaucrat->decrementGrade();
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	delete bureaucrat;
+
+	Bureaucrat *bureaucrat2 = new Bureaucrat("Bureaucrat2", 1);
+	std::cout << *bureaucrat2 << std::endl;
+	// incrementGrade => exception "Grade is too high"
+	try {
+		bureaucrat2->incrementGrade();
+	}
+	catch (std::exception & e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
+}
