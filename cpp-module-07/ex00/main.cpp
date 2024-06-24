806d961ab501a498325da2f860984ce6a68d3009
Loup Quehec <lquehec@made-f0Ar1s7.clusters.42paris.fr>
2024-06-24 21:00:19 +0200

    init

--- cpp-module-07/ex00/main.cpp
+++ cpp-module-07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lquehec <lquehec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 12:43:29 by lquehec           #+#    #+#             */
-/*   Updated: 2024/06/17 15:37:22 by lquehec          ###   ########.fr       */
+/*   Updated: 2024/06/24 20:13:00 by lquehec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,5 +30,15 @@ int	main(void)
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	// test with string and int (not possible)
+	// std::string e = "chaine1";
+	// int			f = 42;
+	
+	// ::swap(e, f);
+	// std::cout << "e = " << e << ", f = " << f << std::endl;
+	// std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	// std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+	
 	return (0);
 }
